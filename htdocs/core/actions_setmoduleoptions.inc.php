f398c01db5bd603576a5d1b70712058af4162f11
stickler-ci <support@stickler-ci.com>
2022-11-14 16:41:54 +0000

    Fixing style errors.

--- htdocs/core/actions_setmoduleoptions.inc.php
+++ htdocs/core/actions_setmoduleoptions.inc.php
@@ -147,7 +147,6 @@ if ($action == 'setModuleOptions' && !empty($user->admin)) {
 
 
 		if ($upload_dir) {
-
 			$result = dol_add_file_process($upload_dir, 1, 1, 'uploadfile', '');
 			if ($result <= 0) {
 				$error++;
